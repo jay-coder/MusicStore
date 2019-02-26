3ef53a8b3bfde4e6ec03c8ad8e1111b58660032d
Jay Lin <linj@mirvac.com>
2019-02-26 14:15:07 +1100

    Init checkin.

---
+++ Projects/IdentityServer/Startup.cs
@@ -0,0 +1,30 @@
+﻿using JayCoder.MusicStore.IdentityServer.Configuration;
+using JayCoder.MusicStore.IdentityServer.Quickstart.UI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JayCoder.MusicStore.IdentityServer
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryIdentityResources(Resources.GetIdentityResources())
+                .AddInMemoryApiResources(Resources.GetApiResources())
+                .AddInMemoryClients(Clients.Get())
+                .AddTestUsers(TestUsers.Users);
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+
+            app.UseIdentityServer();
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
